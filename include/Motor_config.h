d254f346d8659b5ac05125829ebde03e3bea9ea4
Sara hossny hassan <104026246+Sara-Hossny@users.noreply.github.com>
2024-05-12 23:43:20 +0300

    add Motor config

---
+++ include/Motor_config.h
@@ -0,0 +1,22 @@
+/*
+ * Motor_confg.h
+ *
+ *  Created on: May 5, 2024
+ *      Author: Rana Hossny
+ *           Sara Hossny
+ */
+
+#ifndef INCLUDE_MOTOR_CONFG_H_
+#define INCLUDE_MOTOR_CONFG_H_
+
+#include "hw_types.h"
+#include "hw_memmap.h"
+#include "hw_sysctl.h"
+
+// Define MOTOR pin configurations
+#define MOTOR_ENABLE_PIN  GPIO_PIN_2
+#define MOTOR_IN1_PIN GPIO_PIN_3
+#define MOTOR_IN2_PIN GPIO_PIN_4
+
+
+#endif /* INCLUDE_MOTOR_CONFG_H_ */
