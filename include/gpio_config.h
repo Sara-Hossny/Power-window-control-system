2d0fb46bbd857cdc1235a194e2488262f95dfc66
Sara hossny hassan <104026246+Sara-Hossny@users.noreply.github.com>
2024-05-12 23:44:05 +0300

    add gpio config

---
+++ include/gpio_config.h
@@ -0,0 +1,17 @@
+/*
+ * gpio_config.h
+ *
+ *  Created on: May 12, 2024
+ *      Author: Rana Hossny
+ *             Sara Hossny
+ */
+
+#ifndef GPIO_CONFIG_H_
+#define GPIO_CONFIG_H_
+
+
+#define GPIO_PIN_SET            1
+
+
+
+#endif /* GPIO_CONFIG_H_ */
