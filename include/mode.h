072a8ba675516a5b90ba2acff7a3034a533c482f
Sara hossny hassan <104026246+Sara-Hossny@users.noreply.github.com>
2024-05-12 23:45:32 +0300

    add mode header

---
+++ include/mode.h
@@ -0,0 +1,15 @@
+/*
+ * mode.h
+ *
+ *  Created on: May 10, 2024
+ *      Author: Rana Hossny
+ *          Sara Hossny
+ */
+
+#ifndef MODE_H_
+#define MODE_H_
+
+#define DEBUG 0
+
+
+#endif /* MODE_H_ */
